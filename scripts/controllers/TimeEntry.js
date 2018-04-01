9932f4e70e8e0fe7e6fa703ba0ee86b9f70f0611
Inna Dudinets <iliadna@gmail.com>
2018-04-01 11:46:25 +0300

    frontend setup

---
+++ scripts/controllers/TimeEntry.js
@@ -0,0 +1,17 @@
+(function() {
+	'use strict';
+	angular
+		.module('timeTracker')
+		.controller('TimeEntry', TimeEntry);
+
+		function TimeEntry(time) {
+			var vm = this;
+			vm.timeentries = [];
+			time.getTime().then(function(results) {
+				vm.timeentries = results;
+				console.log(vm.timeentries);
+			}, function(error) {
+				console.log(error);
+			});
+		}
+})();
